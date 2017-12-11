0d887b7909f6c0b5f7194b24ec5d6e6380afc19c
Michael Billington <michael.billington@gmail.com>
2017-12-11 21:45:36 +1100

    Update interwiki to use human-readable JSON over serialized PHP format

--- script/interwiki-update.sh
+++ script/interwiki-update.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+set -eu
+
 # Get interwiki table from mediawiki.org. This is a sane default for most sites.
-url='http://www.mediawiki.org/w/api.php?action=query&meta=siteinfo&siprop=interwikimap&format=php'
-dest='../src/interwiki.ser'
-wget -O $dest $url
+url="https://www.mediawiki.org/w/api.php?action=query&meta=siteinfo&siprop=interwikimap&format=json"
+dest="../src/Mike42/Wikitext/interwiki.json"
+curl "$url" | json_reformat > "$dest"
