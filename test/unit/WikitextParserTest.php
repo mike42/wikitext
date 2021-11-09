78819f0afa2c400404f7842d7252127196256802
Trismegiste <flo@trismegiste.net>
2021-11-09 19:46:46 +0100

    d.r.y

--- test/unit/WikitextParserTest.php
+++ test/unit/WikitextParserTest.php
@@ -17,24 +17,20 @@ class WikitextParserTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals($expected, $this->sut->parse($wikitext));
     }
 
-    public function testParse()
+    public function example(): array
     {
-        $this->assertParsingEquals("<p>Testing 1 2 3</p>\n", "Testing 1 2 3");
+        return [
+            ["<p>Testing 1 2 3</p>\n", "Testing 1 2 3"],
+            ["<p>Link : <a href=\"yolo\" title=\"yolo\">yolo</a></p>\n", "Link : [[yolo]]"],
+            ["<table>\n<tr>\n<td>aaa</td>\n</tr>\n</table>\n", "{|\n|aaa\n|}"],
+            ["<p><a href=\"hungary\"><img src=\"Flag of Hungary vertical.jpg\" alt=\"yolo\" /></a></p>\n", "[[File:Flag of Hungary vertical.jpg|bottom|8px|link=hungary|alt=yolo]]"]
+        ];
     }
 
-    public function testLink()
+    /** @dataProvider example */
+    public function testExample($expected, $wikitext)
     {
-        $this->assertParsingEquals("<p>Link : <a href=\"yolo\" title=\"yolo\">yolo</a></p>\n", "Link : [[yolo]]");
-    }
-
-    public function testTable()
-    {
-        $this->assertParsingEquals("<table>\n<tr>\n<td>aaa</td>\n</tr>\n</table>\n", "{|\n|aaa\n|}");
-    }
-
-    public function testImage()
-    {
-        $this->assertParsingEquals("<p><a href=\"hungary\"><img src=\"Flag of Hungary vertical.jpg\" alt=\"yolo\" /></a></p>\n", "[[File:Flag of Hungary vertical.jpg|bottom|8px|link=hungary|alt=yolo]]");
+        $this->assertParsingEquals($expected, $wikitext);
     }
 
 }
